cce6067a83879ac243cbc6456be84a0b60e46688
Kelly Rebeca <33762542+kellyberreca@users.noreply.github.com>
2017-12-12 08:29:57 -0300

    Update retangulo.lua

--- funcoes/retangulo.lua
+++ funcoes/retangulo.lua
@@ -1,3 +1,3 @@
 function toqueretangulo(mousex,mousey,rx, ry, rw, rh)
-  return (mousex>= rx and mousex <= rx+rw) and (mousey >= ry and mousey <= ry + rh)
+	return (mousex>= rx and mousex <= rx+rw) and (mousey >= ry and mousey <= ry + rh)
 end
